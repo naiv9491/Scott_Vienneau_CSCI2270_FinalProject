a4e996f8330ee49445f175550bdf97b4d4a7a0e6
MICHAEL VIENNEAU <michael.vienneau@colorado.edu>
2015-04-23 14:40:11 -0600

    Added the breadthFirstTraversal algorythm and updated the main file accordingly

--- GraphExpansion_GlobalLandmarks_CSCI2270/main.cpp
+++ GraphExpansion_GlobalLandmarks_CSCI2270/main.cpp
@@ -11,14 +11,14 @@ int main()
     string strLine;     //Variable to store each line of the text file
     string *landmarks;     //Dynamic array to store each landmark
     int **landmarkDistanceValues;      //Dynamic two dimensional array to store distances between landmarks
-
-
+    
+    
     int numLines = -1;  //Variable to store the number of lines in the file
-
+    
     Graph g;    //Create an instance of the graph class
-
+    
     inFile.open("Landmarks.txt");    //Open the file to read in the data
-
+    
     if(inFile.fail())
         cout << "File did not open." << endl;
     else
@@ -29,25 +29,25 @@ int main()
             getline(inFile, strLine);
             numLines++;
         }
-
+        
         //Initialize both dynamic arrays
         landmarks = new string [numLines-1];
         landmarkDistanceValues = new int *[numLines-1]; //Initialize first dimension of two dimension array
         //Initialize second dimension of 2d array
         for(int i = 0; i < numLines-1; i++)
             landmarkDistanceValues[i] = new int[numLines-1];
-
+        
         //Clear the file and reset to beginning of file to read in data
         inFile.clear();
         inFile.seekg(0, ios::beg);
-
+        
         //Get the first line of the file
         getline(inFile, strLine);
-
+        
         //Find all of the commas in the first line
         int comma1 = strLine.find(',');
         int comma2 = strLine.find(',', comma1+1);
-
+        
         for(int i = 0; i < numLines-2; i++)
         {
             //Loop through just the first line adding in all landmarks to the landmarks array
@@ -55,22 +55,22 @@ int main()
             comma1 = comma2;
             comma2 = strLine.find(',', comma2+1);
         }
-
-
+        
+        
         //Create variable line length to get the last landmark name in the file without getting the extra \r sequence
         int lineLength = strLine.substr(comma1+1).length();
         landmarks[numLines-2] = strLine.substr(comma1+1, lineLength-1);
-
+        
         int index = 0;  //Variable to keep track of all of the lines traversed in the file
-
+        
         while(inFile.good())
         {
             //Get line in file
             getline(inFile, strLine);
-
+            
             int comma3 = strLine.find(',');
             int comma4 = strLine.find(',', comma3+1);
-
+            
             //Loop through the remaining lines and add all numerical values to the 2d array
             //Update the comma values for each numerical value
             for(int i = 0; i < numLines-1; i++)
@@ -79,23 +79,23 @@ int main()
                 comma3 = comma4;
                 comma4 = strLine.find(',', comma4+1);
             }
-
+            
             index++;
             if(index == numLines-1)
                 break;
-
+            
         }
     }
-
+    
     inFile.close();
-
+    
     //Add all landmarks to the graph as vertices
     for(int k = 0; k < numLines-1; k++)
     {
         g.addVertex(landmarks[k]);
     }
-
-
+    
+    
     //Loop through the 2d array and add all edges on the graph between vertices
     for(int i = 0; i < numLines-1; i++)
     {
@@ -107,127 +107,128 @@ int main()
                 g.addEdge(landmarks[i], landmarks[j], landmarkDistanceValues[i][j]);
                 //If one edge was already created, set the corresponding value to -10 so two edges are not created for the same vertices
                 landmarkDistanceValues[j][i] = -10;
-
+                
             }
         }
     }
-
-    LOOP:while(1 == 1)
+    
+LOOP:while(1 == 1)
+{
+    //Print out the main menu with all options
+    cout << "===== Main Menu =====" << endl;
+    cout << "1. Print Vertices" << endl;
+    cout << "2. Find Shortest Path" << endl;
+    cout << "3. Find Shortest Distance" << endl;
+    cout << "4. Primm's Minimum Spanning Tree Algorithm" << endl;
+    cout << "5. Brute Force Traveling Salesman Problem" << endl;
+    cout << "6. Quit" << endl;
+    
+    //Get user input on what option to run
+    string userInput = "";
+    getline(cin, userInput);
+    
+    if(userInput == "1")
     {
-        //Print out the main menu with all options
-        cout << "===== Main Menu =====" << endl;
-        cout << "1. Print Vertices" << endl;
-        cout << "2. Find Shortest Path" << endl;
-        cout << "3. Find Shortest Distance" << endl;
-        cout << "4. Primm's Minimum Spanning Tree Algorithm" << endl;
-        cout << "5. Brute Force Traveling Salesman Problem" << endl;
-        cout << "6. Quit" << endl;
-
-        //Get user input on what option to run
-        string userInput = "";
+        //Print out the vertices and corresponding edges
+        g.displayEdges();   //Call the display edges method
+    }
+    
+    else if(userInput == "2")
+    {
+        //Print sub-menu to show which shortest path algorithm
+        cout << endl << "1. Breadth-First Search" << endl;
+        cout << "2. Depth-First Search Recursive" << endl;
+        cout << "3. Depth-First Search Non-Recursive" << endl;
+        
+        //Get user input on which shortest path algorithm
+        userInput = "";
         getline(cin, userInput);
-
+        
+        //Two variables to keep track of each city
+        string city1;
+        string city2;
+        
         if(userInput == "1")
         {
-            //Print out the vertices and corresponding edges
-            g.displayEdges();   //Call the display edges method
-        }
-
-        else if(userInput == "2")
-        {
-            //Print sub-menu to show which shortest path algorithm
-            cout << endl << "1. Breadth-First Search" << endl;
-            cout << "2. Depth-First Search Recursive" << endl;
-            cout << "3. Depth-First Search Non-Recursive" << endl;
-
-            //Get user input on which shortest path algorithm
-            userInput = "";
-            getline(cin, userInput);
-
-            //Two variables to keep track of each city
-            string city1;
-            string city2;
-
-            if(userInput == "1")
-            {
-                //Find the shortest path between two cities using breadth-first
-                cout << "Enter a starting city:" << endl;
-                getline(cin, city1);
-                cout << "Enter an ending city:" << endl;
-                getline(cin, city2);
-                userInput = ""; //Reset the value of userInput
-            }
-            else if(userInput == "2")
-            {
-                //Find the shortest path between two cities using depth-first recursive
-                cout << "Enter a starting city:" << endl;
-                getline(cin, city1);
-                cout << "Enter an ending city:" << endl;
-                getline(cin, city2);
-                userInput = "";
-            }
-            else if(userInput == "3")
-            {
-                //Find the shortest path between two cities using breadth-first
-                cout << "Enter a starting city:" << endl;
-                getline(cin, city1);
-                cout << "Enter an ending city:" << endl;
-                getline(cin, city2);
-                userInput = ""; //Reset the value of user input
-            }
-            else
-            {
-                cout << "Invalid Input, restarting from Main Menu" << endl;
-                goto LOOP;  //Go back to main menu
-            }
-        }
-
-        else if(userInput == "3")
-        {
-            //Find shortest distance between two cities using Dijkstra's algorithm
-            string city1;
-            string city2;
+            //Find the shortest path between two cities using breadth-first
             cout << "Enter a starting city:" << endl;
             getline(cin, city1);
             cout << "Enter an ending city:" << endl;
             getline(cin, city2);
+            userInput = ""; //Reset the value of userInput
+            g.breadthFirstSearch(city1, city2);
         }
-
-        else if(userInput == "4")
+        else if(userInput == "2")
         {
-            //Find the minimum spanning tree using Primm
-            string city1;
+            //Find the shortest path between two cities using depth-first recursive
             cout << "Enter a starting city:" << endl;
             getline(cin, city1);
-            userInput = ""; //Reset the value of the user input
-
-        }
-
-        else if(userInput == "5")
-        {
-            //Solve the traveling salesman problem using brute force
-            string city1;
-            cout << "Enter a starting city:" << endl;
-            getline(cin, city1);
+            cout << "Enter an ending city:" << endl;
+            getline(cin, city2);
             userInput = "";
-
         }
-
-        else if(userInput == "6")
+        else if(userInput == "3")
         {
-            //If user wants to quit
-            cout << "Goodbye!" << endl;
-            return 0;
+            //Find the shortest path between two cities using breadth-first
+            cout << "Enter a starting city:" << endl;
+            getline(cin, city1);
+            cout << "Enter an ending city:" << endl;
+            getline(cin, city2);
+            userInput = ""; //Reset the value of user input
         }
-
         else
         {
-            //If the input is invalid
             cout << "Invalid Input, restarting from Main Menu" << endl;
-            goto LOOP;
+            goto LOOP;  //Go back to main menu
         }
-
     }
-
+    
+    else if(userInput == "3")
+    {
+        //Find shortest distance between two cities using Dijkstra's algorithm
+        string city1;
+        string city2;
+        cout << "Enter a starting city:" << endl;
+        getline(cin, city1);
+        cout << "Enter an ending city:" << endl;
+        getline(cin, city2);
+    }
+    
+    else if(userInput == "4")
+    {
+        //Find the minimum spanning tree using Primm
+        string city1;
+        cout << "Enter a starting city:" << endl;
+        getline(cin, city1);
+        userInput = ""; //Reset the value of the user input
+        
+    }
+    
+    else if(userInput == "5")
+    {
+        //Solve the traveling salesman problem using brute force
+        string city1;
+        cout << "Enter a starting city:" << endl;
+        getline(cin, city1);
+        userInput = "";
+        
+    }
+    
+    else if(userInput == "6")
+    {
+        //If user wants to quit
+        cout << "Goodbye!" << endl;
+        return 0;
+    }
+    
+    else
+    {
+        //If the input is invalid
+        cout << "Invalid Input, restarting from Main Menu" << endl;
+        goto LOOP;
+    }
+    
+}
+    
     return 0;
 }
--- GraphExpansion_GlobalLandmarks_CSCI2270/src/Graph.cpp
+++ GraphExpansion_GlobalLandmarks_CSCI2270/src/Graph.cpp
@@ -1,6 +1,7 @@
 #include "Graph.h"
 #include <string>
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -15,26 +16,26 @@ Graph::~Graph()
 }
 
 /*
-Function Prototype:
-void Graph::addVertex(string)
-
-Function Description:
-This method takes in a name for a landmark and checks to make sure a vertex has not already been associate with that name. If it has not, it will create
-a new vertex with the name and add it to the vertices vector
-
-Example:
-Graph g;
-g.addVector("Eiffel Tower");
-
-Precondition: The string parameter has no extra spaces, and is the correct name of the landmark. The struct vertex has been declared with name and distance variables.
-The vertices vector has been declared.
-
-Postcondition: The string name given will be created into a vertex and added to the vertices vector. If the name was already created as a vector the program will say that name was found.
-*/
+ Function Prototype:
+ void Graph::addVertex(string)
+ 
+ Function Description:
+ This method takes in a name for a landmark and checks to make sure a vertex has not already been associate with that name. If it has not, it will create
+ a new vertex with the name and add it to the vertices vector
+ 
+ Example:
+ Graph g;
+ g.addVector("Eiffel Tower");
+ 
+ Precondition: The string parameter has no extra spaces, and is the correct name of the landmark. The struct vertex has been declared with name and distance variables.
+ The vertices vector has been declared.
+ 
+ Postcondition: The string name given will be created into a vertex and added to the vertices vector. If the name was already created as a vector the program will say that name was found.
+ */
 void Graph::addVertex(string name)
 {
     bool found = false; //Variable to check if the vertex has been already created
-
+    
     //Loop through all of the vertices
     for(int i = 0; i < vertices.size(); i++)
     {
@@ -45,7 +46,7 @@ void Graph::addVertex(string name)
             cout<< vertices[i].name<<" found."<<endl;
         }
     }
-
+    
     //If the name was not already associated with a vertex
     //Add it to vertices and update the vertex information via the vertex struct
     if(found == false){
@@ -57,24 +58,24 @@ void Graph::addVertex(string name)
 }
 
 /*
-Function Prototype:
-void Graph::addEdge(string, string, int)
-
-Function Description:
-This method takes in two names for landmarks, finds if the two names correspond to vertices, and adds an edge between
-the vertices with the corresponding parameter for weight.
-
-Example:
-Graph g;
-g.addEdge("Eiffel Tower", "Taj Mahal", 2000);
-
-Precondition: The vertices vector and the adjacent vector have been declared. The struct for adjVertex has been initialized with a pointer to the vector and the weight.
-If no movies have been added to the vertices vector, then v1 and v2 will not be found. The starting landmark is string v1 and the ending landmark is v2.
-The weight between the landmarks will be given by int weight. All this information can be found in the generated text file.
-
-Postcondition: An edge will be added between the two vertices with the corresponding weight. The adjacent vector for v1 will have v2 added, and the adjacent vector
-for v2 will have v1 added. The edge will be created in both directions.
-*/
+ Function Prototype:
+ void Graph::addEdge(string, string, int)
+ 
+ Function Description:
+ This method takes in two names for landmarks, finds if the two names correspond to vertices, and adds an edge between
+ the vertices with the corresponding parameter for weight.
+ 
+ Example:
+ Graph g;
+ g.addEdge("Eiffel Tower", "Taj Mahal", 2000);
+ 
+ Precondition: The vertices vector and the adjacent vector have been declared. The struct for adjVertex has been initialized with a pointer to the vector and the weight.
+ If no movies have been added to the vertices vector, then v1 and v2 will not be found. The starting landmark is string v1 and the ending landmark is v2.
+ The weight between the landmarks will be given by int weight. All this information can be found in the generated text file.
+ 
+ Postcondition: An edge will be added between the two vertices with the corresponding weight. The adjacent vector for v1 will have v2 added, and the adjacent vector
+ for v2 will have v1 added. The edge will be created in both directions.
+ */
 void Graph::addEdge(string v1, string v2, int weight)
 {
     //Loop through all of the vertices
@@ -105,21 +106,21 @@ void Graph::addEdge(string v1, string v2, int weight)
 }
 
 /*
-Function Prototype:
-void Graph::displayEdges()
-
-Function Description:
-This method takes in no parameters and displays all vertices with their corresponding adjacent vertices
-
-Example:
-Graph g;
-g.displayEdges();
-
-Precondition: The vertices vector has to be declared and the vertex struct has to be declared and initialized with the name variable. The adjacent vertices vector has to
-be declared as well with the pointer to the vertex struct
-
-Postcondition: All vertices will be displayed with a following list displaying all adjacent vertices to the original vertex.
-*/
+ Function Prototype:
+ void Graph::displayEdges()
+ 
+ Function Description:
+ This method takes in no parameters and displays all vertices with their corresponding adjacent vertices
+ 
+ Example:
+ Graph g;
+ g.displayEdges();
+ 
+ Precondition: The vertices vector has to be declared and the vertex struct has to be declared and initialized with the name variable. The adjacent vertices vector has to
+ be declared as well with the pointer to the vertex struct
+ 
+ Postcondition: All vertices will be displayed with a following list displaying all adjacent vertices to the original vertex.
+ */
 void Graph::displayEdges()
 {
     //Loop through all vertices
@@ -127,7 +128,7 @@ void Graph::displayEdges()
     {
         //cout << vertices[i].name << endl;
         cout << vertices[i].name << "-->";
-
+        
         //For each vertex, loop through all adjacent vertices
         for(int j = 0; j < vertices[i].adj.size(); j++)
         {
@@ -135,8 +136,97 @@ void Graph::displayEdges()
             if(j != vertices[i].adj.size()-1)
                 cout << "***";
         }
-
+        
         cout << endl << endl;
-
+        
     }
+}
+/*
+ Function Prototype:
+ void Graph::breadthFirstSearch(string, string)
+ 
+ Function Description:
+ This method takes in two parameters and returns the shortest path between the two cities
+ 
+ Example:
+ Graph g;
+ g.breadthFirstSearch("Stonehenge", "Golden Gate Bridge");
+ 
+ Precondition: The vertices vector has to be declared and the vertex struct has to be declared and initialized with the name variable. The adjacent vertices vector has to
+ be declared as well with the pointer to the vertex struct
+ 
+ Postcondition: Display the integer for the shortest path, and the string representation of the path taken
+ */
+void Graph::breadthFirstSearch(string city1, string city2){
+    vector<vertex> path;
+    int distance;
+    vertex *s = NULL;
+    vertex *e = NULL;
+    vertex u;
+    bool found1 = false;
+    bool found2 = false;
+    bool dist = true;
+    queue<queueVertex> myQueue;
+    for (int i = 0; i < vertices.size(); i++){
+        if (vertices[i].name == city1){
+            found1 = true;
+            s = &vertices[i];
+        }else if (vertices[i].name == city2){
+            found2 = true;
+            e = &vertices[i];
+        }
+    }
+    if (found1 && found2){
+        for (int i = 0; i < vertices.size(); i++){
+            if (vertices[i].name != s->name){
+                vertices[i].visited = false;
+            }else{
+                vertices[i].visited = true;
+            }
+            
+        }
+        s->visited = true;
+        queueVertex qv;
+        qv.distance = 0;
+        qv.path.push_back(*s);
+        myQueue.push(qv);
+        while (!(myQueue.empty())){
+            qv = myQueue.front();
+            myQueue.pop();
+            vertex *v = NULL;
+            int end = qv.path.size();
+            for (int i = 0; i < qv.path[end-1].adj.size(); i++){
+                v = qv.path[end-1].adj[i].v;
+                if (v->visited == false){
+                    path = qv.path;
+                    v->visited = true;
+                    distance = qv.distance + 1;
+                    path.push_back(*v);
+                    queueVertex temp;
+                    temp.path = path;
+                    temp.distance = distance;
+                    if (v->name == city2){
+                        //return temp;
+                        cout << temp.distance;
+                        for (int j = 0; j < temp.path.size(); j++){
+                            cout << "," << temp.path[j].name;
+                        }
+                    }else {
+                        myQueue.push(temp);
+                    }
+                    
+                }
+            }
+        }
+        cout << endl;
+        
+    }
+    
+    
+    if (found1 == false || found2 == false){
+        cout << "One or more cities doesn't exist" << endl;
+    }
+    
+    
+    
 }
